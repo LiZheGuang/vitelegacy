72db15d0be37dbef65277939e327f7ad5c6101c4
LiZheGuang <1030660726@qq.com>
2021-03-01 23:17:28 +0800

    feat: 增加ES5语法兼容

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
+// https://vitejs.dev/config/
+export default defineConfig({
+  mode: 'development',
+  plugins: [vue(), legacy({
+    targets: ["defaults",
+      "not IE 11",
+      "maintained node versions"]
+  })]
+})
